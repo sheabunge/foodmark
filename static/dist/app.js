aa9accb1bc25fe2964c465562b0c6ae488176620
Shea Bunge <info@bungeshea.com>
2016-08-28 13:26:37 +1000

    Add dist files to repository

---
+++ static/dist/app.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var a=document.querySelector(".toggle-night"),b=document.body.classList,c=document.getElementById("ground"),d=function(){a.innerHTML='<span class="fa fa-moon-o"></span>',b.remove("night"),b.add("day"),c.src="/static/images/ground.svg"},e=function(){a.innerHTML='<span class="fa fa-sun-o"></span>',b.remove("day"),b.add("night"),c.src="/static/images/ground-night.svg"};a.onclick=function(){document.body.classList.contains("night")?d():e()},d()}();
+//# sourceMappingURL=app.js.map
